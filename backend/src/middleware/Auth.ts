5b1cabfa00e24bee7656bc5f29ea0b55fe5a5b42
Madina Sharopova <madinasharopova308@gmail.com>
2023-08-28 11:21:13 +0500

    sjdhbjahjehfjshef

--- backend/src/middleware/Auth.ts
+++ backend/src/middleware/Auth.ts
@@ -1,10 +1,10 @@
 import { RequestHandler } from "express";
 import createHttpError from "http-errors";
 
-export const requiresAuth:RequestHandler = (req,res,next) => {
-    if(req.session.userId){
+export const requiresAuth: RequestHandler = (req, res, next) => {
+    if (req.session.userId) {
         next()
-    }else{
-        next(createHttpError(401,"User is not authenticated"))
+    } else {
+        next(createHttpError(401, "User is not authenticated"))
     }
 }
